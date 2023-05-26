69e3529b5be817f762ae0a3e77b5fcd18ea2f594
Jonatán Pribék <pribek.jonatan@edu.bme.hu>
2023-05-26 19:01:34 +0200

    Ground and jumper's body

--- source/GrafikaHaziCsomag/Programs/Skeleton/Skeleton/Skeleton.cpp
+++ source/GrafikaHaziCsomag/Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -32,13 +32,14 @@
 // negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
 //=============================================================================================
 
+//A feladatot a 3D motorkából kiindulva oldottam meg.
+//https://edu.vik.bme.hu/mod/url/view.php?id=97498
+
 #include "framework.h"
 
-//---------------------------
-template<class T> struct Dnum { // Dual numbers for automatic derivation
-//---------------------------
-	float f; // function value
-	T d;  // derivatives
+template<class T> struct Dnum {
+	float f;
+	T d;
 	Dnum(float f0 = 0, T d0 = T(0)) { f = f0, d = d0; }
 	Dnum operator+(Dnum r) { return Dnum(f + r.f, d + r.d); }
 	Dnum operator-(Dnum r) { return Dnum(f - r.f, d - r.d); }
@@ -50,7 +51,6 @@ template<class T> struct Dnum { // Dual numbers for automatic derivation
 	}
 };
 
-// Elementary functions prepared for the chain rule as well
 template<class T> Dnum<T> Exp(Dnum<T> g) { return Dnum<T>(expf(g.f), expf(g.f)*g.d); }
 template<class T> Dnum<T> Sin(Dnum<T> g) { return  Dnum<T>(sinf(g.f), cosf(g.f)*g.d); }
 template<class T> Dnum<T> Cos(Dnum<T>  g) { return  Dnum<T>(cosf(g.f), -sinf(g.f)*g.d); }
@@ -67,18 +67,16 @@ typedef Dnum<vec2> Dnum2;
 
 const int tessellationLevel = 20;
 
-//---------------------------
-struct Camera { // 3D camera
-//---------------------------
-	vec3 wEye, wLookat, wVup;   // extrinsic
-	float fov, asp, fp, bp;		// intrinsic
+struct Camera {
+	vec3 wEye, wLookat, wVup;
+	float fov, asp, fp, bp;
 public:
 	Camera() {
 		asp = (float)windowWidth / windowHeight;
 		fov = 75.0f * (float)M_PI / 180.0f;
 		fp = 1; bp = 20;
 	} 
-	mat4 V() { // view matrix: translates the center to the origin
+	mat4 V() {
 		vec3 w = normalize(wEye - wLookat);
 		vec3 u = normalize(cross(wVup, w));
 		vec3 v = cross(w, u);
@@ -88,7 +86,7 @@ public:
 			                                       0,   0,   0,   1);
 	}
 
-	mat4 P() { // projection matrix
+	mat4 P() {
 		return mat4(1 / (tan(fov / 2)*asp), 0,                0,                      0,
 			        0,                      1 / tan(fov / 2), 0,                      0,
 			        0,                      0,                -(fp + bp) / (bp - fp), -1,
@@ -96,23 +94,17 @@ public:
 	}
 };
 
-//---------------------------
 struct Material {
-//---------------------------
 	vec3 kd, ks, ka;
 	float shininess;
 };
 
-//---------------------------
 struct Light {
-//---------------------------
 	vec3 La, Le;
-	vec4 wLightPos; // homogeneous coordinates, can be at ideal point
+	vec4 wLightPos;
 };
 
-//---------------------------
 struct RenderState {
-//---------------------------
 	mat4	           MVP, M, Minv, V, P;
 	Material *         material;
 	std::vector<Light> lights;
@@ -120,9 +112,7 @@ struct RenderState {
 	vec3	           wEye;
 };
 
-//---------------------------
 class Shader : public GPUProgram {
-//---------------------------
 public:
 	virtual void Bind(RenderState state) = 0;
 
@@ -140,9 +130,7 @@ public:
 	}
 };
 
-//---------------------------
 class GouraudShader : public Shader {
-//---------------------------
 	const char * vertexSource = R"(
 		#version 330
 		precision highp float;
@@ -186,7 +174,6 @@ class GouraudShader : public Shader {
 		}
 	)";
 
-	// fragment shader in GLSL
 	const char * fragmentSource = R"(
 		#version 330
 		precision highp float;
@@ -202,7 +189,7 @@ public:
 	GouraudShader() { create(vertexSource, fragmentSource, "fragmentColor"); }
 
 	void Bind(RenderState state) {
-		Use(); 		// make this program run
+		Use();
 		setUniform(state.MVP, "MVP");
 		setUniform(state.M, "M");
 		setUniform(state.Minv, "Minv");
@@ -216,16 +203,54 @@ public:
 	}
 };
 
-//---------------------------
+class GroundShader : public Shader {
+	const char* vertexSource = R"(
+		#version 330
+		precision highp float;
+
+		uniform mat4  MVP;
+
+		layout(location = 0) in vec3  vtxPos;            // pos in modeling space
+		layout(location = 1) in vec3  vtxNorm;      	 // normal in modeling space
+
+		out vec3 radiance;		    // reflected radiance
+
+		void main() {
+			gl_Position = vec4(vtxPos, 1) * MVP;
+			radiance = vec3(abs(vtxPos.y/2), abs(vtxPos.y/5), 0);
+			if (vtxPos.y < 0)
+				radiance = vec3(0, abs(vtxPos.y), 0);
+		}
+	)";
+
+	const char* fragmentSource = R"(
+		#version 330
+		precision highp float;
+
+		in  vec3 radiance;      // interpolated radiance
+		out vec4 fragmentColor; // output goes to frame buffer
+
+		void main() {
+			fragmentColor = vec4(radiance, 1);
+		}
+	)";
+public:
+	GroundShader() { create(vertexSource, fragmentSource, "fragmentColor"); }
+
+	void Bind(RenderState state) {
+		Use();
+		setUniform(state.MVP, "MVP");
+	}
+};
+
 class Geometry {
-//---------------------------
 protected:
-	unsigned int vao, vbo;        // vertex array object
+	unsigned int vao, vbo;
 public:
 	Geometry() {
 		glGenVertexArrays(1, &vao);
 		glBindVertexArray(vao);
-		glGenBuffers(1, &vbo); // Generate 1 vertex buffer object
+		glGenBuffers(1, &vbo);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	}
 	virtual void Draw() = 0;
@@ -235,9 +260,7 @@ public:
 	}
 };
 
-//---------------------------
 class ParamSurface : public Geometry {
-//---------------------------
 	struct VertexData {
 		vec3 position, normal;
 		vec2 texcoord;
@@ -247,14 +270,16 @@ class ParamSurface : public Geometry {
 public:
 	ParamSurface() { nVtxPerStrip = nStrips = 0; }
 
-	virtual void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z) = 0;
+	virtual void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z, float phif1, float phif2) = 0;
 
 	VertexData GenVertexData(float u, float v) {
 		VertexData vtxData;
 		vtxData.texcoord = vec2(u, v);
 		Dnum2 X, Y, Z;
 		Dnum2 U(u, vec2(1, 0)), V(v, vec2(0, 1));
-		eval(U, V, X, Y, Z);
+		float phif1 = (rand() % 10 + 1) / 100.0f;
+		float phif2 = (rand() % 10 + 1) / 100.0f;
+		eval(U, V, X, Y, Z, phif1, phif2);
 		vtxData.position = vec3(X.f, Y.f, Z.f);
 		vec3 drdU(X.d.x, Y.d.x, Z.d.x), drdV(X.d.y, Y.d.y, Z.d.y);
 		vtxData.normal = cross(drdU, drdV);
@@ -264,7 +289,7 @@ public:
 	void create(int N = tessellationLevel, int M = tessellationLevel) {
 		nVtxPerStrip = (M + 1) * 2;
 		nStrips = N;
-		std::vector<VertexData> vtxData;	// vertices on the CPU
+		std::vector<VertexData> vtxData;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j <= M; j++) {
 				vtxData.push_back(GenVertexData((float)j / M, (float)i / N));
@@ -272,11 +297,9 @@ public:
 			}
 		}
 		glBufferData(GL_ARRAY_BUFFER, nVtxPerStrip * nStrips * sizeof(VertexData), &vtxData[0], GL_STATIC_DRAW);
-		// Enable the vertex attribute arrays
-		glEnableVertexAttribArray(0);  // attribute array 0 = POSITION
-		glEnableVertexAttribArray(1);  // attribute array 1 = NORMAL
-		glEnableVertexAttribArray(2);  // attribute array 2 = TEXCOORD0
-		// attribute array, components/attribute, component type, normalize?, stride, offset
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		glEnableVertexAttribArray(2);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, position));
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, normal));
 		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, texcoord));
@@ -289,22 +312,42 @@ public:
 };
 
 
-//---------------------------
-class Torus : public ParamSurface {
-//---------------------------
+class Body1 : public ParamSurface {
 public:
-	Torus() { create(); }
-	void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z) {
-		const float R = 1, r = 0.5f;
-		U = U * 2.0f * M_PI, V = V * 2.0f * M_PI;
-		Dnum2 D = Cos(U) * r + R;
-		X = D * Cos(V); Y = D * Sin(V); Z = Sin(U) * r;
+	Body1() { create(); }
+	void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z, float phif1, float phif2) {
+		X = V; Y = U;
 	}
 };
 
-//---------------------------
+class Body2 : public ParamSurface {
+public:
+	Body2() { create(); }
+	void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z, float phif1, float phif2) {
+		X = V; Z = U;
+	}
+};
+
+class Body3 : public ParamSurface {
+public:
+	Body3() { create(); }
+	void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z, float phif1, float phif2) {
+		Y = V; Z = U;
+	}
+};
+
+
+class Ground : public ParamSurface {
+public:
+	Ground() { create(8, 8); }
+	void eval(Dnum2& f1, Dnum2& f2, Dnum2& X, Dnum2& Y, Dnum2& Z, float phif1, float phif2) {
+		X = 20 * f1.f;
+		Y = 0.3f / sqrtf(powf(f1.f, 2) + powf(f2.f, 2)) * cos(f1.f * X.f + f2.f * Y.f + phif1 * phif2);
+		Z = 20 * f2.f;
+	}
+};
+
 struct Object {
-//---------------------------
 	Shader *   shader;
 	Material * material;
 	Geometry * geometry;
@@ -335,48 +378,72 @@ public:
 	}
 };
 
-//---------------------------
 class Scene {
-//---------------------------
 	std::vector<Object *> objects;
 	Camera camera;
 	std::vector<Light> lights;
 public:
 	void Build() {
-		// Shaders
 		Shader * gouraudShader = new GouraudShader();
+		Shader * groundShader = new GroundShader();
 
-		// Materials
 		Material * material0 = new Material;
 		material0->kd = vec3(0.6f, 0.4f, 0.2f);
 		material0->ks = vec3(4, 4, 4);
 		material0->ka = vec3(0.1f, 0.1f, 0.1f);
 		material0->shininess = 100;
 
-		// Geometries
-		Geometry * torus = new Torus();
+		Material* material1 = new Material;
+		material1->kd = vec3(0.5f, 0.5f, 0.5f);
+		material1->ks = vec3(10, 10, 10);
+		material1->ka = vec3(0.8f, 0.6f, 0.0f);
+		material1->shininess = 5;
 
-		// Create objects by setting up their vertex data on the GPU
+		Geometry* body1 = new Body1();
+		Geometry* body2 = new Body2();
+		Geometry* body3 = new Body3();
+		Geometry* ground = new Ground();
 
-		Object * torusObject1 = new Object(gouraudShader, material0, torus);
-		torusObject1->translation = vec3(0, 0, 0);
-		torusObject1->scale = vec3(0.7f, 0.7f, 0.7f);
-		torusObject1->rotationAxis = vec3(1, 0, 0);
-		objects.push_back(torusObject1);
+		Object * bodyObject1 = new Object(gouraudShader, material0, body1);
+		bodyObject1->translation = vec3(0, 1, 0);
+		objects.push_back(bodyObject1);
 
-		// Lights
-		lights.resize(3);
-		lights[0].wLightPos = vec4(5, 5, 4, 0);	// ideal point -> directional light source
-		lights[0].La = vec3(0.1f, 0.1f, 1);
+		Object* bodyObject2 = new Object(gouraudShader, material0, body1);
+		bodyObject2->translation = vec3(0, 1, 1);
+		objects.push_back(bodyObject2);
+		
+		Object* bodyObject3 = new Object(gouraudShader, material0, body2);
+		bodyObject3->translation = vec3(0, 2, 0);
+		objects.push_back(bodyObject3);
+
+		Object* bodyObject4 = new Object(gouraudShader, material0, body2);
+		bodyObject4->translation = vec3(0, 1, 0);
+		objects.push_back(bodyObject4);
+
+		Object* bodyObject5 = new Object(gouraudShader, material0, body3);
+		bodyObject5->translation = vec3(1, 1, 0);
+		objects.push_back(bodyObject5);
+
+		Object* bodyObject6 = new Object(gouraudShader, material0, body3);
+		bodyObject6->translation = vec3(0, 1, 0);
+		objects.push_back(bodyObject6);
+
+		Object* groundObject = new Object(groundShader, material1, ground);
+		groundObject->translation = vec3(-5, -2, -5);
+		objects.push_back(groundObject);
+
+		lights.resize(5);
+		lights[0].wLightPos = vec4(0, 1, 0, 0);
+		lights[0].La = vec3(0.5f, 0.5f, 0.5f);
 		lights[0].Le = vec3(3, 0, 0);
 
-		lights[1].wLightPos = vec4(5, 10, 20, 0);	// ideal point -> directional light source
-		lights[1].La = vec3(0.2f, 0.2f, 0.2f);
-		lights[1].Le = vec3(0, 3, 0);
+		lights[1].wLightPos = vec4(4, 0, 4, 1);
+		lights[1].La = vec3(0.5f, 0.5f, 0.5f);
+		lights[1].Le = vec3(3, 0, 0);
 
-		lights[2].wLightPos = vec4(-5, 5, 5, 0);	// ideal point -> directional light source
-		lights[2].La = vec3(0.1f, 0.1f, 0.1f);
-		lights[2].Le = vec3(0, 0, 3);
+		lights[2].wLightPos = vec4(-4, 0, -1, 1);
+		lights[2].La = vec3(0.5f, 0.5f, 0.5f);
+		lights[2].Le = vec3(3, 0, 0);
 	}
 
 	void Render(Camera camera) {
@@ -399,13 +466,13 @@ void onInitialization() {
 	glEnable(GL_DEPTH_TEST);
 	glDisable(GL_CULL_FACE);
 	scene.Build();
-	spinCamera.wEye = vec3(0, 0, 4);
-	spinCamera.wLookat = vec3(0, 0, -1);
+	spinCamera.wEye = vec3(0, 1, 4);
+	spinCamera.wLookat = vec3(0, 1, -1);
 	spinCamera.wVup = vec3(0, 1, 0);
 
-	jumperCamera.wEye = vec3(0, 3, 3);
-	jumperCamera.wLookat = vec3(0, -1, -1);
-	jumperCamera.wVup = vec3(0, 1, -1);
+	jumperCamera.wEye = vec3(0, 6, 0);
+	jumperCamera.wLookat = vec3(0, -1, 0);
+	jumperCamera.wVup = vec3(0, 0, 1);
 }
 
 void onDisplay() {
@@ -425,8 +492,8 @@ void onIdle() {
 	static float dt = 0;
 	dt = (glutGet(GLUT_ELAPSED_TIME) - animateStartTime) / 1000.0f;
 	if (animateStart) {
-		spinCamera.wEye = vec3(-sinf(dt), 0, cosf(dt)) * 4;
-		spinCamera.wLookat = vec3(-sinf(dt), 0, cosf(dt));
+		spinCamera.wEye = vec3(-sinf(dt), 0.25f, cosf(dt)) * 4;
+		spinCamera.wLookat = vec3(-sinf(dt), 1, cosf(dt));
 	}
 	glutPostRedisplay();
 }
